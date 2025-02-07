6e4d7cee09ed512c86275ef476f065baa35fd41f
Thomas <123959293+thomas-stecinski@users.noreply.github.com>
2025-02-07 15:21:12 +0100

    retirer ssl

--- MovieSpot_Backend/src/app.module.ts
+++ MovieSpot_Backend/src/app.module.ts
@@ -21,9 +21,6 @@ import { Reservation } from './reservation/entities/reservation.entity';
       database: process.env.DB_NAME,
       entities: [User, Reservation], 
       synchronize: true,
-      ssl: {
-        rejectUnauthorized: false, // ✅ Ajoute cette ligne pour éviter les problèmes SSL sur Render
-      },
     }),
     UserModule, 
     AuthModule,
